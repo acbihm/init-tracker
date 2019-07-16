a06f9c956cc0f2f857b59456219b8abe5c3eccfa
Alex C. B <43941153+acbihm@users.noreply.github.com>
2019-07-15 19:59:11 -0500

    UI tweaks

--- init-tracker/src/components/List/index.js
+++ init-tracker/src/components/List/index.js
@@ -8,16 +8,22 @@ class List extends React.Component {
                 <ul class="list-group">
                     <li class="list-group-item PC active"><span class="name">Player char 1</span>
                         <span class="tracker"></span></li>
+                        
                     <li class="list-group-item PC"><span class="name">Player char 2</span>
                         <span class="tracker"></span></li>
+                        
                     <li class="list-group-item enemy"><span class="name">Enemy</span>
                         <span class="tracker"></span></li>
+                        
                     <li class="list-group-item empty"><span class="name">Please enter a name</span>
                         <span class="tracker"></span></li>
+                        
                     <li class="list-group-item empty"><span class="name">Please enter a name</span>
                         <span class="tracker"></span></li>
+                        
                     <li class="list-group-item empty"><span class="name">Please enter a name</span>
                         <span class="tracker"></span></li>
+                        
                 </ul>
                 <button class="add-char">
                     Add character
